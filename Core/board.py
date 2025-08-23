d54bd2aedfeb769a4910a83f216f4bca1d686a69
Santiago Ortega <santy.ortega010@gmail.com>
2025-08-23 11:25:10 -0300

    armado del tablero

--- Core/board.py
+++ Core/board.py
@@ -1,3 +1,20 @@
-Class board: 
+class Board:
     def __init__(self):
-        self.__board__ = self.create_board()
+        self.__board__ = self.create_board() 
+        
+    def create_board(self):
+        
+        board = [0] * 24
+        board[0] = 2
+        board[5] = -5
+        board[7] = -3
+        board[11] = 5
+        board[12] = -5
+        board[16] = 3
+        board[18] = 5
+        board[23] = -2
+        return board
+
+
+
+

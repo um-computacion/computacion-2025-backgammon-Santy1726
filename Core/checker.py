05a88befc8fdf61a228520de7a4f74a6098f3421
Santiago Ortega <santy.ortega010@gmail.com>
2025-08-25 19:28:18 -0300

    se cambiaron nombre de los atributos

--- Core/checker.py
+++ Core/checker.py
@@ -1,10 +1,10 @@
 class Checker:
     def __init__(self, color, position):
-        self.color = color
-        self.position = position
+        self.__color__ = color
+        self.__position__ = position
 
     def move(self, new_position):
-        self.position = new_position
+        self.__position__ = new_position
 
     def is_off_board(self):
-        return self.position == 'off'
+        return self.__position__ == 'off'

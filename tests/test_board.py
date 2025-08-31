ffebed3c7bd0963509513222352bf0d9137219a3
Santiago Ortega <santy.ortega010@gmail.com>
2025-08-31 10:08:56 -0300

    agregado de posibles pruebas de test

--- tests/test_board.py
+++ tests/test_board.py
@@ -5,3 +5,13 @@ class TestTablero(unittest.TestCase):
 
     def setUp(self):
         self.board = Board()
+
+    def test_crear_board(self):
+        board_esperado = [2, 0, 0, 0, 0, -5, 0, -3, 0, 0, 0, 5, -5, 0, 0, 0, 3, 0, 5, 0, 0, 0, 0, -2]
+        self.assertEqual(self.board.obtener_board(), board_esperado)
+
+    def test_establecer_posicion(self):
+        self.board.establecer_posicion(0, 3)
+        self.assertEqual(self.board.obtener_posicion(0), 3)
+        self.board.establecer_posicion(5, -2)
+        self.assertEqual(self.board.obtener_posicion(5), -2)

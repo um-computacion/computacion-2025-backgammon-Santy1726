ef64827e8743cccbcf403f0d96321766f18a1ba4
Santiago Ortega <santy.ortega010@gmail.com>
2025-09-08 21:20:09 -0300

    arreglos en el codigo checker

--- Core/checker.py
+++ Core/checker.py
@@ -1,10 +1,25 @@
 class Checker:
-    def __init__(self, color, position):
+    def __init__(self, color, posicion):
+
         self.__color__ = color
-        self.__position__ = position
+        self.__posicion__ = posicion
 
-    def move(self, new_position):
-        self.__position__ = new_position
+    def mover(self, nueva_posicion):
+    
+        self.__posicion__ = nueva_posicion
 
-    def is_off_board(self):
-        return self.__position__ == 'off'
+    def esta_fuera(self):
+        
+        return self.__posicion__ == 'off'
+
+    def obtener_color(self):
+    
+        return self.__color__
+
+    def obtener_posicion(self):
+  
+        return self.__posicion__
+
+    def __str__(self):
+     
+        return f"Ficha {self.__color__} en {self.__posicion__}"

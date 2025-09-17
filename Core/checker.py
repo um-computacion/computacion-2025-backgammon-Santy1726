7162ae68b18e85f80b4dbacc04b57040d7cb13fd
Santiago Ortega <santy.ortega010@gmail.com>
2025-09-16 22:16:45 -0300

    arreglos en la clase checker

--- Core/checker.py
+++ Core/checker.py
@@ -1,22 +1,22 @@
 class Checker:
-    def __init__(self, color, posicion):
+    def __init__(self, color: str, posicion: int):
 
         self.__color__ = color
         self.__posicion__ = posicion
 
-    def mover(self, nueva_posicion):
+    def mover(self, nueva_posicion: int):
     
         self.__posicion__ = nueva_posicion
 
-    def esta_fuera(self):
+    def esta_fuera(self) -> bool:   
         
         return self.__posicion__ == 'off'
 
-    def obtener_color(self):
+    def obtener_color(self) -> str:
     
         return self.__color__
 
-    def obtener_posicion(self):
+    def obtener_posicion(self) -> int:
   
         return self.__posicion__
 

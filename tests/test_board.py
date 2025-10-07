5cb18fcdb9b6245a43ba96f3ee08a6172027c6bc
Santiago Ortega <santy.ortega010@gmail.com>
2025-10-07 17:19:10 -0300

    cambio y arreglo del test para el board

--- tests/test_board.py
+++ tests/test_board.py
@@ -1,37 +1,70 @@
 import unittest
 from Core.board import Board
+from Core.checker import Checker
+
 
 class TestBoard(unittest.TestCase):
 
     def setUp(self):
         self.board = Board()
 
-    def test_crear_board(self):
-        board_esperado = [2, 0, 0, 0, 0, -5, 0, -3, 0, 0, 0, 5, -5, 0, 0, 0, 3, 0, 5, 0, 0, 0, 0, -2]
-        self.assertEqual(self.board.obtener_board(), board_esperado)
+    def test_inicializacion_correcta(self):
+        self.assertEqual(len(self.board.__board__), 24)
 
-    def test_establecer_posicion(self):
-        self.board.establecer_posicion(0, 3)
-        self.assertEqual(self.board.obtener_posicion(0), 3)
-        self.board.establecer_posicion(5, -2)
-        self.assertEqual(self.board.obtener_posicion(5), -2)
+        self.assertEqual(len(self.board.__board__[0]), 2)
+        self.assertEqual(len(self.board.__board__[5]), 5)
+        self.assertEqual(len(self.board.__board__[7]), 3)
+        self.assertEqual(len(self.board.__board__[11]), 5)
+        self.assertEqual(len(self.board.__board__[12]), 5)
+        self.assertEqual(len(self.board.__board__[16]), 3)
+        self.assertEqual(len(self.board.__board__[18]), 5)
+        self.assertEqual(len(self.board.__board__[23]), 2)
 
-    def test_obtener_posicion(self):
-        self.assertEqual(self.board.obtener_posicion(0), 2)
+    def test_mover_ficha_valido(self):
+        self.board.__board__[2] = [Checker("blanco", 2)]
+        self.board.mover_ficha(2, 3)
 
-        with self.assertRaises(IndexError):
-            self.board.obtener_posicion(24)
+        self.assertEqual(len(self.board.__board__[2]), 0)
+        self.assertEqual(len(self.board.__board__[3]), 1)
+        self.assertEqual(self.board.__board__[3][0].obtener_posicion(), 3)
 
-    def test_mover_ficha(self):
-        self.board.mover_ficha(0, 1)
-        self.assertEqual(self.board.obtener_posicion(0), 1)
-        self.assertEqual(self.board.obtener_posicion(1), 1)
-
-        with self.assertRaises(IndexError):
-            self.board.mover_ficha(0, 24)
-
+    def test_mover_ficha_sin_fichas(self):
         with self.assertRaises(ValueError):
             self.board.mover_ficha(2, 3)
 
-if __name__ == '__main__':
+    def test_mover_ficha_fuera_de_rango(self):
+        with self.assertRaises(IndexError):
+            self.board.mover_ficha(-1, 2)
+
+        with self.assertRaises(IndexError):
+            self.board.mover_ficha(0, 25)
+
+    def test_captura_simple(self):
+        self.board.__board__[0] = [Checker("blanco", 0)]
+        self.board.__board__[1] = [Checker("negro", 1)]
+
+        self.board.mover_ficha(0, 1)
+
+        bar_negro = self.board.__bar__["negro"]
+        self.assertEqual(len(bar_negro), 1)
+        self.assertEqual(bar_negro[0].obtener_color(), "negro")
+        self.assertEqual(bar_negro[0].obtener_posicion(), "bar")
+
+        self.assertEqual(len(self.board.__board__[1]), 1)
+        self.assertEqual(self.board.__board__[1][0].obtener_color(), "blanco")
+
+    def test_obtener_posicion_devuelve_copia(self):
+        casilla = self.board.obtener_posicion(0)
+        casilla.append("modificado")
+
+        self.assertNotIn("modificado", self.board.__board__[0])
+
+    def test_str_representacion(self):
+        tablero_str = str(self.board)
+        self.assertIn("0:", tablero_str)
+        self.assertIn("23:", tablero_str)
+        self.assertIsInstance(tablero_str, str)
+
+
+if __name__ == "__main__":
     unittest.main()
